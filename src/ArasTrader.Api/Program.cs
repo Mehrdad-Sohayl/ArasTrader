94435f127c9bef0f9c3be8175c461ff2a9b3e368
Mehrdad <MehSohayl@gmail.com>
2026-06-21 00:29:13 +0330

    feat: initialize solution structure

---
+++ src/ArasTrader.Api/Program.cs
@@ -0,0 +1,19 @@
+using ArasTrader.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddInfrastructure(builder.Configuration);
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
